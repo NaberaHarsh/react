671fafb3ec45c8b46d63fc0cb657a70926fb5c7e
Harsh Nabera <harshnabera30@gmail.com>
2019-06-03 20:12:24 +0530

    with progress update

--- src/project.js
+++ src/project.js
@@ -5,276 +5,286 @@ import './project.css';
 //import { arrayExpression } from '@babel/types';
 import './paper.css'
 import axios from 'axios';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import SwipeToDelete from 'react-swipe-to-delete-component';
 
 
 
-class Project extends React.Component{
-    constructor(props){
-      super(props);
-      this.state={}; 
-      this.state.item=[]
-      this.state.newItemName={};
-      this.state.number=0;
-      this.state.per=0;
-      this.state.num=0;
-      this.setStatus=this.setStatus.bind(this);
-      this.getValue=this.getValue.bind(this);
-      this.setValue=this.setValue.bind(this);
-      this.Progress=this.Progress.bind(this);
-      this.completed=this.completed.bind(this);
-      
-          
-      // this.Sort=this.Sort.bind(this);
-      // this.state = {
-      //   sortDirection: 'descending',
-      //   data: this.state.data.sort(descending)
+class Project extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+    this.state.item = [];
+    this.state.newItemName = {};
+    this.state.number = 0;
+    this.state.per = 0;
+    this.state.num = 0;
+    this.setStatus = this.setStatus.bind(this);
+    this.getValue = this.getValue.bind(this);
+    this.setValue = this.setValue.bind(this);
+    this.Progress = this.Progress.bind(this);
+    this.completed = this.completed.bind(this);
+
+
+    // this.Sort=this.Sort.bind(this);
+    // this.state = {
+    //   sortDirection: 'descending',
+    //   data: this.state.data.sort(descending)
     // };
-           
-      
-    }
 
-  setStatus(ele){
-let p=this.state.item;
-let i=p.indexOf(ele);
-p[i].status = !p[i].status;
 
-axios.put('http://localhost:8080/update/'+p[i]._id, p[i])
-.then((res)=>{
-  console.log(res.data);
-})
-  this.setState({
-    item:p
+  }
+
+  setStatus(ele) {
+    let p = this.state.item;
+    let i = p.indexOf(ele);
+    p[i].status = !p[i].status;
+
+    axios.put('http://localhost:8080/update/' + p[i]._id, p[i])
+      .then((res) => {
+        console.log(res.data);
+      })
+    this.setState({
+      item: p
     })
 
-    let count=this.state.number;
-        if(this.state.item[i].status){
+    let count = this.state.number;
+    if (this.state.item[i].status) {
       count++;
-            this.setState(
-        {number:count}
+      this.setState(
+        { number: count }
       )
     }
-    else{
+    else {
       count--;
       this.setState(
-        {number:count}
+        { number: count }
       )
-      }
-      let numb=count/this.state.item.length;
-      console.log(numb);
-      this.setState(
-      {per:numb*100}
-      ) 
-  this.state.num=count;
-  this.setState({
-    num:count
-  })
-
-      }
-      
-
-  Progress(){    
-    let numb=this.state.num/this.state.item.length;
-     console.log(numb);
-     this.setState(
-     {per:numb*100}
-     )
-        
-  }
-      
-getList(props){
-let items=[];
-items=this.state.item.map((ele,i)=> <ListItem key={i} ele={ele} setStatus={this.setStatus} up={()=>this.up(i)} down={()=>this.down(i)} delete={()=>this.delete(i)} GoToTop={()=>{this.GoToTop(i)}} Progress={()=>{this.Progress()} }  ></ListItem> )
-
-return items;
-
     }
-
-getValue(e){ 
-   this.newItemName= e.target.value
-}
-
-setValue(e){
-
-    if(this.newItemName){
-    console.log(e.target.value)
-  let d=new Date();
-    let obj= {name:this.newItemName,status:false,date:d.toLocaleString()}
-  
-    axios.post('http://localhost:8080/task',obj)
-    .then((res)=>{
-      console.log(res.data);
-          })
- 
-    let l=this.state.item;
-    l.push(obj)
+    let numb = count / this.state.item.length;
+    console.log(numb);
     this.setState(
-    {item:l}
-          )
-          this.newItemName="";
-          this.Progress();
-        }     
-    document.getElementById("aa").value='';
-  }
- 
-   up=(i)=>{
-     let x=this.state.item;
-    if(i!=0)
-    {
-      [x[i],x[i-1]]=[x[i-1],x[i]]
-    }
-    else
-    alert("This item is already at Top");
-this.setState({
-  item:x
-})
-  }
-  
-  down=(i)=>{
-    let x=this.state.item;
-    if(i!=this.state.item.length-1)
-    {
-      [x[i],x[i+1]]=[x[i+1],x[i]]
-    }
-    
-    else
-    alert("This item is already at Bottom");
-this.setState({
-  item:x
-})
-  }
-
-  delete(i){
-    alert("Are you sure you want to Delete");
-    let count=this.state.number;
-    if(this.state.item[i].status)
-    count--;
-    this.setState(
-      {number:count}
+      { per: numb * 100 }
     )
-    let x=this.state.item;
-
-    axios.delete('http://localhost:8080/delete/'+x[i]._id)
-    .then((res)=>{
-      console.log(res.data);
-    })
-
-      x.splice(i,1);
+    this.state.num = count;
     this.setState({
-      item:x
+      num: count
     })
-    let numb=count/this.state.item.length;
-     console.log(numb);
-     this.setState(
-     {per:numb*100}
-     )
 
   }
 
-  GoToTop(i){
-    let y=this.state.item;
-    [y[i],y[0]]=[y[0],y[i]]
-    console.log(i);
-    for(let j=i; j>1;j--){
-    [y[i-1],y[j]]=[y[j],y[i-1]]
-  i--;  
+
+  Progress() {
+    let numb = this.state.num / this.state.item.length;
+    console.log(numb);
+    this.setState(
+      { per: numb * 100 }
+    )
+
   }
+
+  getList(props) {
+    let items = [];
+    items = this.state.item.map((ele, i) => <ListItem key={i} ele={ele} setStatus={this.setStatus} up={() => this.up(i)} down={() => this.down(i)} delete={() => this.delete(i)} GoToTop={() => { this.GoToTop(i) }} Progress={() => { this.Progress() }}  ></ListItem>)
+
+    return items;
+
+  }
+
+  getValue(e) {
+    this.newItemName = e.target.value
+  }
+
+  setValue(e) {
+
+    if (this.newItemName) {
+      console.log(e.target.value)
+      let d = new Date();
+      let obj = { name: this.newItemName, status: false, date: d.toLocaleString() }
+
+      axios.post('http://localhost:8080/task', obj)
+        .then((res) => {
+          console.log(res.data);
+        })
+
+      let l = this.state.item;
+      l.push(obj)
+      this.setState(
+        { item: l }
+      )
+      this.newItemName = "";
+      this.Progress();
+    }
+    document.getElementById("aa").value = '';
+  }
+
+  up = (i) => {
+    let x = this.state.item;
+    if (i != 0) {
+      [x[i], x[i - 1]] = [x[i - 1], x[i]]
+    }
+    else
+      alert("This item is already at Top");
     this.setState({
-      item:y
+      item: x
     })
-}
+  }
 
-// sortAscending = () => {
-//   const { item } = this.state;
-//   item.sort((a, b) => a - b)    
-//   this.setState({ item })
-// }
+  down = (i) => {
+    let x = this.state.item;
+    if (i != this.state.item.length - 1) {
+      [x[i], x[i + 1]] = [x[i + 1], x[i]]
+    }
 
-// sortDescending = () => {
-//   const { item } = this.state;
-//   item.sort((a, b) => a - b).reverse()
-//   this.setState({ item })
-// }
+    else
+      alert("This item is already at Bottom");
+    this.setState({
+      item: x
+    })
+  }
 
-//   sortData() {
-//     if(this.state.sortDirection ==='descending') {
-//         this.setState({ 
-//             sortDirection: 'ascending',
-//             data: this.props.item.sort(sortAscending)
-//         });
-//     } else {
-//         this.setState({ 
-//             sortDirection: 'descending',
-//             data: this.props.item.sort(sortDescending)
-//         });
-//     }
-// }
+  delete(i) {
+    alert("Are you sure you want to Delete");
+    let count = this.state.number;
+    if (this.state.item[i].status)
+      count--;
+    this.setState(
+      { number: count }
+    )
+    let x = this.state.item;
 
-completed(){
-//  let count=0;
-//   for(let i of this.props.item)
-//   {
-//     if(i.status)
-//   {count++;}
-// }
-  console.log("0");
-
-}
-
-componentDidMount(){
-  axios.get('http://localhost:8080/read')
-  .then((res)=>{
-      console.log(res.data);
-      this.setState({
-        item:res.data
+    axios.delete('http://localhost:8080/delete/' + x[i]._id)
+      .then((res) => {
+        console.log(res.data);
       })
-      let count=this.state.number;
-      for(let i=1;i<this.state.item.length;i++){
-      if(this.state.item[i].status)
-      console.log("working");
-      // {count++;
-      //   this.setState({
-      //     number:count
-      //   })
-      // }
-      }
-  })
- 
 
-}
+    x.splice(i, 1);
+    this.setState({
+      item: x
+    })
+    let numb = count / this.state.item.length;
+    console.log(numb);
+    this.setState(
+      { per: numb * 100 }
+    )
 
-render(){
+  }
+
+  GoToTop(i) {
+    let y = this.state.item;
+    [y[i], y[0]] = [y[0], y[i]]
+    console.log(i);
+    for (let j = i; j > 1; j--) {
+      [y[i - 1], y[j]] = [y[j], y[i - 1]]
+      i--;
+    }
+    this.setState({
+      item: y
+    })
+  }
+
+  // sortAscending = () => {
+  //   const { item } = this.state;
+  //   item.sort((a, b) => a - b)    
+  //   this.setState({ item })
+  // }
+
+  // sortDescending = () => {
+  //   const { item } = this.state;
+  //   item.sort((a, b) => a - b).reverse()
+  //   this.setState({ item })
+  // }
+
+  //   sortData() {
+  //     if(this.state.sortDirection ==='descending') {
+  //         this.setState({ 
+  //             sortDirection: 'ascending',
+  //             data: this.props.item.sort(sortAscending)
+  //         });
+  //     } else {
+  //         this.setState({ 
+  //             sortDirection: 'descending',
+  //             data: this.props.item.sort(sortDescending)
+  //         });
+  //     }
+  // }
+
+  completed() {
+    //  let count=0;
+    //   for(let i of this.props.item)
+    //   {
+    //     if(i.status)
+    //   {count++;}
+    // }
+    console.log("0");
+
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/read')
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          item: res.data
+        }, () => {
+          let count=this.state.number;
+          for (let i = 0; i < this.state.item.length; i++) {
+            if (this.state.item[i].status)
+              count++;
+              this.setState({
+                number:count
+              },()=>{
+                let numb = count/ this.state.item.length;
+          console.log(numb);
+          this.setState(
+            { per: numb * 100 }
+          )
+              }
+              )
+          }
+          
+          
+          
+        })
+      })
+
+
+
+  }
+
+  render() {
     return <div>
       <h2 className="row flex-center">ToDo List</h2>
-      <div style={{display:'flex'}}>
-  <input id="aa"  onChange={(ele)=>{this.getValue(ele)}}  ></input>
-  
-  <button onClick={this.setValue.bind(this)} className="btn-outline-info" id="dd">ADD ITEM</button>
-  </div>
-      <p className="row flex-center" style={{marginTop:20}}>Completed Items:{this.state.number} /{this.state.item.length}</p>
+      <div style={{ display: 'flex' }}>
+        <input id="aa" onChange={(ele) => { this.getValue(ele) }}  ></input>
+
+        <button onClick={this.setValue.bind(this)} className="btn-outline-info" id="dd">ADD ITEM</button>
+      </div>
+      <p className="row flex-center" style={{ marginTop: 20 }}>Completed Items:{this.state.number} /{this.state.item.length}</p>
       <div className="progress" id="bar">
-  <div className="progress-bar  progress-bar-striped bg-warning progress-bar-animated" role="progressbar" style={{width:`${this.state.per}%`}}></div>
-          </div> 
+        <div className="progress-bar  progress-bar-striped bg-warning progress-bar-animated" role="progressbar" style={{ width: `${this.state.per}%` }}></div>
+      </div>
       <br></br>
-      <ul>{this.getList()}</ul>  
-          
-     </div>
+      <ul>{this.getList()}</ul>
+
+    </div>
   }
 }
 
-class ListItem extends React.Component{
-  constructor(props){
+class ListItem extends React.Component {
+  constructor(props) {
     super(props);
   }
-  
-render(){
-  return <div style={{display: 'flex'}} className="col-sm-12">
-  <li onClick={()=>{this.props.setStatus(this.props.ele) ; this.props.Progress()}} onDoubleClick={(i)=>{this.props.GoToTop()}} id="cc" className={this.props.ele.status ? " paper-btn btn-block btn-success": "paper-btn btn-block"} >{this.props.ele.name}<div style={{marginLeft :650, position: 'absolute'}}>{this.props.ele.date}</div></li>
-  <button onClick={(i)=>this.props.up()} className="btn-warning" >Up</button>
-  <button onClick={(i)=>this.props.down()}  className="btn-info">Down</button>
-  <button onClick={(i)=>this.props.delete()}  className="btn-danger">X</button>
-  </div> }
+
+  render() {
+    return <div style={{ display: 'flex' }} className="col-sm-12">
+      <li onClick={() => { this.props.setStatus(this.props.ele); this.props.Progress() }} onDoubleClick={(i) => { this.props.GoToTop() }} id="cc" className={this.props.ele.status ? " paper-btn btn-block btn-success" : "paper-btn btn-block"} >{this.props.ele.name}<div style={{ marginLeft: 650, position: 'absolute' }}>{this.props.ele.date}</div></li>
+      <button onClick={(i) => this.props.up()} className="btn-warning" >Up</button>
+      <button onClick={(i) => this.props.down()} className="btn-info">Down</button>
+      <button onClick={(i) => this.props.delete()} className="btn-danger">X</button>
+    </div>
+  }
 }
 
 // class Button extends React.Component{
@@ -296,7 +306,7 @@ render(){
 //       <button onClick={(e)=>this.UpShift(e)}>up</button><button>down</button><button>X</button>
 //     </div>
 //   }
-  
+
 //   }
 
 
